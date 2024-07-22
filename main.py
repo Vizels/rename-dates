3d09228ad1a5d2c85c97e7804f051d21424abb54
Vizels <mirchuk.georgiy@gmail.com>
2024-07-22 13:33:01 +0200

    Added some verification in main

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ path = "/mnt/f/test/"
 file_list = os.listdir(path)
 
 base = FileName()
-
 try:
     for file_name in file_list:
         base.read_file_name(file_name)
-        base.rename_file()
-        os.rename(path + file_name, path + base.rename_file())
+        if os.path.isdir(path + file_name) and base.is_base_digital():
+            print(base.base)
+            base.rename_file()
+            os.rename(path + file_name, path + base.rename_file())
+            
     print("Files renamed successfully.")
 except Exception as e:
     print(e)
