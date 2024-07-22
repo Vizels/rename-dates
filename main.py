a2b4fd52b0036a515d5733f119df0b13a9f33b41
Vizels <mirchuk.georgiy@gmail.com>
2024-07-22 13:03:14 +0200

    Refactored

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ file_list = os.listdir(path)
 
 base = FileName()
 
-for file_name in file_list:
-    print(base.read_file_name(file_name))
-    print(base.rename_file())
-    os.rename(path + file_name, path + base.rename_file())
+try:
+    for file_name in file_list:
+        base.read_file_name(file_name)
+        base.rename_file()
+        os.rename(path + file_name, path + base.rename_file())
+    print("Files renamed successfully.")
+except Exception as e:
+    print(e)
+    print("Error in renaming files.")
--- name.py
+++ name.py
@@ -1,41 +1,56 @@
 class FileName:
-    def __init__(self, init_format = ['MM', 'DD', 'YYYY'], delimiter = '-'):
-        
+    """
+    A class for manipulating and renaming file names based on a given format.
+    """
+
+    def __init__(
+        self, init_format=["MM", "DD", "YYYY"], delimiter="-", custom_base=None
+    ):
+        """
+        Initializes a FileName object.
+
+        Args:
+            init_format (list): The initial format of the file name. Defaults to ['MM', 'DD', 'YYYY'].
+            delimiter (str): The delimiter used to separate the parts of the file name. Defaults to '-'.
+        """
         self.init_format = init_format
+        self.delimiter = delimiter
 
-        self.delitimiter = delimiter
+        if custom_base:
+            self.base = custom_base
+        else:
+            self.base = {"MM": None, "DD": None, "YYYY": None}
 
-        self.base = {
-                'MM': None,
-                'DD': None,
-                'YYYY': None
-            }
-        
         self.name = None
-        
+
     def read_file_name(self, name):
+        """
+        Reads a file name and extracts the parts based on the initial format.
+
+        Args:
+            name (str): The file name to be processed.
+
+        Returns:
+            dict: A dictionary containing the extracted parts of the file name.
+        """
         self.name = name
-
-        parts = name.split(self.delitimiter)
-
+        parts = name.split(self.delimiter)
         for i in range(len(parts)):
             self.base[self.init_format[i]] = parts[i]
-        
         return self.base
-    
-    def rename_file(self, new_format = ['YYYY', 'MM', 'DD'], new_delimiter = '-'):
-        
+
+    def rename_file(self, new_format=["YYYY", "MM", "DD"], new_delimiter="-"):
+        """
+        Renames the file based on a new format and delimiter.
+
+        Args:
+            new_format (list): The new format of the file name. Defaults to ['YYYY', 'MM', 'DD'].
+            new_delimiter (str): The new delimiter used to separate the parts of the file name. Defaults to '-'.
+
+        Returns:
+            str: The renamed file name.
+        """
         new_name = []
-        
         for i in range(len(new_format)):
             new_name.append(self.base[new_format[i]])
-        
         return new_delimiter.join(new_name)
-
-        
-        
-
-
-
-
-        
